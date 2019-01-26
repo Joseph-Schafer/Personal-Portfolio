5ff10e2a61419622550fda4447ee977f65dca6c6
Joey Schafer <45374243+Joseph-Schafer@users.noreply.github.com>
2019-01-26 10:50:27 -0800

    Added files created for Lox Interpreter
    These files were created by me while following along with the tutorial book "Crafting Interpreters" by Robert Nystrom. They together make an interpreter for the Lox invented programming language, which includes several standard programming language features such as conditionals, loops, functions, and OOP elements.

---
+++ Lox/Lox.java
@@ -0,0 +1,90 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+//This is a test to see if cyberduck works
+public class Lox {
+   private static final Interpreter interpreter = new Interpreter();
+   static boolean hadError = false;
+   static boolean hadRuntimeError = false;
+
+   public static void main(String[] args) throws IOException {
+      if (args.length > 1) {
+         System.out.println("Usage: jlox [script]");
+         System.exit(64);
+      } else if (args.length == 1) {
+         runFile(args[0]);
+      } else {
+        runPrompt();
+      }
+   }
+
+   //Runs a given Lox program file specified by the given path
+   private static void runFile(String path) throws IOException {
+      byte[] bytes = Files.readAllBytes(Paths.get(path));
+      run(new String(bytes, Charset.defaultCharset()));
+
+      //Indicate error in exit code
+      if (hadError) System.exit(65);
+      if (hadRuntimeError) System.exit(70);
+   }
+
+   //Method for executing a Lox REPL loop
+   private static void runPrompt() throws IOException {
+      InputStreamReader input = new InputStreamReader(System.in);
+      BufferedReader reader = new BufferedReader(input);
+
+      for (;;) {
+         System.out.print("> ");
+         run(reader.readLine());
+         hadError = false;
+      }
+   }
+
+   //Runs given source code in Lox
+   private static void run(String source) {
+      Scanner scanner = new Scanner(source);
+      List<Token> tokens = scanner.scanTokens();
+
+      Parser parser = new Parser(tokens);
+      List<Stmt> statements = parser.parse();
+
+      if (hadError) return;
+
+      Resolver resolver = new Resolver(interpreter);
+      resolver.resolve(statements);
+
+      if (hadError) return;
+
+      interpreter.interpret(statements);
+   }
+
+   static void error(int line, String message) {
+      report(line, "", message);
+   }
+
+   private static void report(int line, String where, String message) {
+      System.err.println(
+         "[line "+line+"] Error " + where + ": " + message);
+      hadError = true;
+   }
+
+   static void error(Token token, String message) {
+     if (token.type == TokenType.EOF) {
+       report(token.line, " at end", message);
+     }else {
+       report(token.line, " at ''"+token.lexeme+"'", message);
+     }
+   }
+
+   static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line "+error.token.line +"]");
+    hadRuntimeError = true;
+   }
+}
